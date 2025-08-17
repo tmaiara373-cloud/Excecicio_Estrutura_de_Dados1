8643ffea6c86285fbc85c11f38c4d9ec232eed54
tmaiara373-cloud <tmaiara373@gmail.com>
2025-08-16 22:18:54 -0300

    Segundo commit - torre de hanoi

---
+++ Aula2/torre_de_hanoi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void hanoi (int n, char origem, char trabalho, char destino){
+    if (n > 0){
+        hanoi(n - 1, origem, destino, trabalho);
+        printf("Mover disco %d de %c para %c\n",n, origem, destino);
+        hanoi(n - 1, trabalho, origem, destino);
+    }
+
+}
+int main (){
+    int discos=3;
+    hanoi(discos, 'A', 'B', 'C');
+    return 0;
+}
+
